21d2c60faa60c35b2f1f93bf1e7c7bd5d92b8582
Leone <leocr_lem@yahoo.com.br>
2022-04-08 17:52:56 -0300

    ok

--- Python_Programmer/exercicioslicao6.py
+++ Python_Programmer/exercicioslicao6.py
@@ -1,28 +1,35 @@
-from codecs import getreader
-from unittest import result
+from logging import exception
 
+def getResultado(estudante ,pontuacao):
+    pontosPossibilidade = 100
+    porcentagem = round(int(pontuacao) / pontosPossibilidade, 2)
+    print(porcentagem)
+   # resultado ="erro"    
+    if 1>= porcentagem >=0.9:
+        resultado = "A"
+    elif 0.9 >= porcentagem >=0.8:
+        resultado = "B"
+    elif 0.8 >= porcentagem >=0.7:
+        resultado = "C"
+    elif 0.7 >= porcentagem >=0.6:
+        resultado = "C" 
+    elif 0.6 >= porcentagem >=0.5:
+        resultado = "D"   
+    elif 0.5 >= porcentagem >=0.4:
+        resultado = "E"
+    elif 0.4 >= porcentagem >=0.3:
+        resultado = "F"  
+    else:
+        resultado = "h"            
 
-pontosPossibilidade = 100
-estudante = input("Digite seu nome:")
-pontuacao = input("DIgite sua pontuacao:")
+    print("Percentual é {}".format(porcentagem))
+    print("A: {} -N: {}".format(estudante, resultado))   
 
-porcentagem = round(pontuacao / pontosPossibilidade, 2)
-resultado ="erro"
-
-if 1>= porcentagem >=0.9:
-    resultado = "A"
-elif 0.9 >= porcentagem >=0.8:
-    resultado = "B"
-elif 0.8 >= porcentagem >=0.7:
-    resultado = "C"
-elif 0.7 >= porcentagem >=0.6:
-    resultado = "C" 
-elif 0.6 >= porcentagem >=0.5:
-    resultado = "D"   
-elif 0.5 >= porcentagem >=0.4:
-    resultado = "E"
-elif 0.4 >= porcentagem >=0.3:
-    resultado = "F"  
-else:
-    resultado = "h"            
+try:
     
+    estudante = input("Digite seu nome:")
+    pontuacao =int(input("DIgite sua pontuacao:"))
+     
+    getResultado(estudante, pontuacao)
+except Exception:
+    print("Digitar numero valido" )
