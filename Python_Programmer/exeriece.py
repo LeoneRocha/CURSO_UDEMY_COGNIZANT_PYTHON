15a3d0341482ed76c1165e3a0eaccf6d7cbbacfd
Leone <leocr_lem@yahoo.com.br>
2022-04-08 15:50:39 -0300

    INTERMEDIrio

---
+++ Python_Programmer/exeriece.py
@@ -0,0 +1,36 @@
+
+lyric = """Can't stop, addicted to the shindig
+Chop top, he says I'm gonna win big
+Choose not a life of imitation
+Distant cousin to the reservation
+Defunct, the pistol that you pay for
+This punk, the feeling that you stay for
+In time, I want to be your best friend
+Eastside love is living on the West End
+Knock out, but boy you better come to
+Don't die, you know the truth is some do
+Go write your message on the pavement
+Burn so bright, I wonder what the wave meant
+White heat is screaming in the jungle
+Complete the motion if you stumble
+Go ask the dust for any answers
+Come back strong with 50 belly dancers"""
+lyric = """Can't stop, addicted to the shindig
+Chop top, he says I'm gonna win big
+Choose not a life of imitation
+Distant cousin to the reservation
+Defunct, the pistol that you pay for
+This punk, the feeling that you stay for
+In time, I want to be your best friend
+Eastside love is living on the West End
+Knock out, but boy you better come to
+Don't die, you know the truth is some do
+Go write your message on the pavement
+Burn so bright, I wonder what the wave meant
+White heat is screaming in the jungle
+Complete the motion if you stumble
+Go ask the dust for any answers
+Come back strong with 50 belly dancers"""
+
+ 
+  
